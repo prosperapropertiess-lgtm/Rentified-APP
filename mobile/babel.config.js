3dd7b5d0cdc5a97d408d5cb09d89f2dfc7f0e578
Ebin Jaison <prosperapropertiess@gmail.com>
2026-08-04 14:52:42 -0400

    fix(babel): Add nativewind/babel plugin for remote EAS production bundler support

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -5,6 +5,7 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
     ],
     plugins: [
+      'nativewind/babel',
       'react-native-reanimated/plugin',
     ],
   };
